ad6fa3ed654c04f3fc69871722c55a71d797d1fb
Tompil3r <tompil3r@gmail.com>
2021-10-07 22:08:26 +0300

    added typenames.h
    # Conflicts:
    #	madnet/ndarray/typenames.h

---
+++ laruen/ndarray/typenames.h
@@ -0,0 +1,14 @@
+
+#ifndef TYPENAMES_H
+#define TYPENAMES_H
+
+#include <vector>
+#include <cstdint>
+
+typedef float float32_t;
+typedef double float64_t;
+typedef std::vector<uint32_t> Shape;
+typedef std::vector<uint64_t> Strides;
+
+
+#endif
