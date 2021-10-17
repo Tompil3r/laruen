b9f0be366f36bf7a447e23a03b74e44c752f0ce2
Tompil3r <tompil3r@gmail.com>
2021-10-17 22:43:23 +0300

    added print function to NDArray

--- laruen/ndarray/ndarray.h
+++ laruen/ndarray/ndarray.h
@@ -6,6 +6,7 @@
 #include <vector>
 #include <cstdint>
 #include <iostream>
+#include <string>
 
 
 template <typename T> class NDArray
@@ -23,31 +24,73 @@ template <typename T> class NDArray
         
         const T* get_data();
 
+
         ~NDArray()
         {
             delete this->data;
         }
+
 
         const Shape& get_shape() const
         {
             return this->shape;
         }
 
+
         const Strides& get_strides() const
         {
             return this->strides;
         }
+
 
         uint8_t get_ndim() const
         {
             return this->ndim;
         }
 
+
         uint64_t get_size() const
         {
             return this->size;
         }
 
+
+        void print(uint8_t dim = 0, uint64_t data_index = 0, bool not_first = false, bool not_last = true) const
+        {
+            uint32_t dim_idx;
+
+            if(not_first) std::cout << std::string(dim, ' '); 
+            std::cout << '[';
+
+            if(dim == this->ndim - 1)
+            {
+                for(dim_idx = 0;dim_idx < this->shape[dim] - 1;dim_idx++)
+                {
+                    std::cout << this->data[data_index + dim_idx] << ", ";
+                }
+
+                std::cout << this->data[data_index + dim_idx] << ']';
+                if(not_last) std::cout << '\n';
+                
+                return;
+            }
+
+            this->print(dim + 1, data_index, false, true);
+            data_index += this->strides[dim];            
+
+            for(dim_idx = 1;dim_idx < this->shape[dim] - 1;dim_idx++)
+            {
+                this->print(dim + 1, data_index, true, true);
+                data_index += this->strides[dim];
+            }
+
+            this->print(dim + 1, data_index, true, false);
+
+            std::cout << ']';
+            
+            if(!dim) std::cout << '\n';
+            else if(not_last) std::cout << std::string(this->ndim - dim, '\n');
+        }
 };
 
 #endif
