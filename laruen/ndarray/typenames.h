4dddabe48bae954bdf6053eaa8cba39f6011d9f1
Tompil3r <tompil3r@gmail.com>
2021-10-28 14:11:31 +0300

    added NDIndex typedef

--- laruen/ndarray/typenames.h
+++ laruen/ndarray/typenames.h
@@ -9,6 +9,7 @@ typedef float float32_t;
 typedef double float64_t;
 typedef std::vector<uint32_t> Shape;
 typedef std::vector<uint64_t> Strides;
+typedef std::vector<uint32_t> NDIndex;
 typedef std::initializer_list<uint32_t> Indices;
 
 
