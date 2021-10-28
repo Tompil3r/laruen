71e66a872a1f9758009600a86b7f18e6c6419423
Tompil3r <tompil3r@gmail.com>
2021-10-28 17:18:30 +0300

    added Range constructor

--- laruen/utils/range.cpp
+++ laruen/utils/range.cpp
@@ -1,2 +1,24 @@
 
+#include "laruen/ndarray/typenames.h"
+#include "laruen/utils/range.h"
 
+#include <cstdint>
+
+template class Range<int8_t>;
+template class Range<uint8_t>;
+template class Range<int16_t>;
+template class Range<uint16_t>;
+template class Range<int32_t>;
+template class Range<uint32_t>;
+template class Range<int64_t>;
+template class Range<uint64_t>;
+template class Range<float32_t>;
+template class Range<float64_t>;
+
+
+template <typename T> Range<T>::Range(T start, T end, T step)
+{
+    this->start = start;
+    this->end = end;
+    this->step = step;
+}
--- laruen/utils/range.h
+++ laruen/utils/range.h
@@ -8,6 +8,10 @@ template <typename T> class Range
     T start;
     T end;
     T step;
+
+
+    public:
+        Range(T start=0, T end=-1, T step=1);
 };
 
 
