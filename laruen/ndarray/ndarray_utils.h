1ff9155dfb1a42d4aa2c13bad0adec9b493629b5
Tompil3r <tompil3r@gmail.com>
2021-11-01 14:51:22 +0200

    added ndarray_utils

---
+++ laruen/ndarray/ndarray_utils.h
@@ -0,0 +1,21 @@
+
+#ifndef NDARRAY_UTILS_H
+#define NDARRAY_UTILS_H
+
+#include "laruen/ndarray/typenames.h"
+#include <cstdint>
+
+
+namespace laruen::ndarray::utils
+{
+    inline uint32_t ceil_index(float64_t index)
+    {
+        return (uint32_t)index + ((uint32_t)index < index);
+    }
+}
+
+
+
+
+
+#endif
