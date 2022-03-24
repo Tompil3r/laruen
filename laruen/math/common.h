17ae663153df595ae3a1615368f9bfaa5798ff3e
Tompil3r <tompil3r@gmail.com>
2022-03-24 12:17:01 +0200

    added constexpr to max and min functions

--- laruen/math/common.h
+++ laruen/math/common.h
@@ -4,12 +4,12 @@
 
 namespace laruen::math::common
 {
-    template <typename T> T max(T value1, T value2)
+    template <typename T> constexpr T max(T value1, T value2)
     {
         return (value1 > value2 ? value1 : value2);
     }
 
-    template <typename T> T min(T value1, T value2)
+    template <typename T> constexpr T min(T value1, T value2)
     {
         return (value1 < value2 ? value1 : value2);
     }
