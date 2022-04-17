d9c967a8358b000af01a7017fb14f72fcb2abf81
Tompil3r <tompil3r@gmail.com>
2022-04-17 16:59:07 +0300

    added BaseArray constructor

--- src/ndarray/base_array.h
+++ src/ndarray/base_array.h
@@ -4,13 +4,30 @@
 
 #include "src/ndarray/ndarray_types.h"
 #include <cstdint>
+#include <utility>
 
 class BaseArray {
-    Shape m_shape;
-    Strides m_strides;
-    uint64_t m_size;
-    uint8_t m_ndim;
-    bool m_free_mem;
+    protected:
+        Shape m_shape;
+        Strides m_strides;
+        uint64_t m_size;
+        uint8_t m_ndim;
+        bool m_free_mem;
+
+
+    public:
+        BaseArray(const Shape &shape) : m_shape(shape),
+        m_strides(shape.size()), m_ndim(shape.size()), m_free_mem(true)
+        {
+            uint64_t stride = 1;
+            this->m_size = (this->m_ndim > 0);
+            
+            for(uint8_t dim = this->m_ndim; dim-- > 0;) {
+                this->m_strides[dim] = stride;
+                this->m_size *= shape[dim];
+                stride *= shape[dim];
+            }
+        }
 };
 
 
