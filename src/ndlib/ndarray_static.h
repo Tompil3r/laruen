26b30b225e10b8a5fd20631e76e10ca7f1763623
Tompil3r <tompil3r@gmail.com>
2022-05-03 12:29:26 +0300

    added add_assign_normal / broadcast to NDArrayStatic

--- src/ndlib/ndarray_static.h
+++ src/ndlib/ndarray_static.h
@@ -6,8 +6,12 @@
 
 class NDArrayStatic {
 
+    public:
+        template <typename T, bool C, typename T2, bool C2>
+        static NDArray<T, C>& add_assign_normal(NDArray<T, C> &lhs, const NDArray<T2, C2> &rhs);
 
-
+        template <typename T, bool C, typename T2, bool C2>
+        static NDArray<T, C>& add_assign_broadcast(NDArray<T, C> &lhs, const NDArray<T2, C2> &rhs);
 };
 
 #include "src/ndlib/ndarray_static.tpp"
