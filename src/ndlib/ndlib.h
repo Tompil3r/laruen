c6f2d37d2c6a9e2c535e69bec99516da0574a108
Tompil3r <tompil3r@gmail.com>
2022-05-04 14:45:17 +0300

    ndlib::multiply_assign_normal / broadcast

--- src/ndlib/ndlib.h
+++ src/ndlib/ndlib.h
@@ -14,6 +14,12 @@ namespace laruen::ndlib {
 
     template <typename T1, bool C1, typename T2, bool C2>
     NDArray<T1, C1>& subtract_assign_broadcast(NDArray<T1, C1> &lhs, const NDArray<T2, C2> &rhs);
+
+    template <typename T1, bool C1, typename T2, bool C2>
+    NDArray<T1, C1>& multiply_assign_normal(NDArray<T1, C1> &lhs, const NDArray<T2, C2> &rhs);
+
+    template <typename T1, bool C1, typename T2, bool C2>
+    NDArray<T1, C1>& multiply_assign_broadcast(NDArray<T1, C1> &lhs, const NDArray<T2, C2> &rhs);
 };
 
 #include "src/ndlib/ndlib.tpp"
