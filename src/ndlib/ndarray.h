19e2c94cc56809b29985ec7b9287cf7b3502787d
Tompil3r <tompil3r@gmail.com>
2022-05-04 14:42:52 +0300

    ndlib:: subtract_assign_normal / broadcast

--- src/ndlib/ndarray.h
+++ src/ndlib/ndarray.h
@@ -146,6 +146,12 @@ namespace laruen::ndlib {
 
             template <typename T1, bool C1, typename T2, bool C2>
             friend NDArray<T1, C1>& add_assign_broadcast(NDArray<T1, C1> &lhs, const NDArray<T2, C2> &rhs);
+
+            template <typename T1, bool C1, typename T2, bool C2>
+            friend NDArray<T1, C1>& subtract_assign_normal(NDArray<T1, C1> &lhs, const NDArray<T2, C2> &rhs);
+
+            template <typename T1, bool C1, typename T2, bool C2>
+            friend NDArray<T1, C1>& subtract_assign_broadcast(NDArray<T1, C1> &lhs, const NDArray<T2, C2> &rhs);
     };
     
     template <typename T, bool C> NDArray(NDArray<T, C>&, const Axes&) -> NDArray<T, false>;
