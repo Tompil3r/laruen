6692b425936d4d38b87a86b006e11dd9fd9e2cf6
Tompil3r <tompil3r@gmail.com>
2022-06-16 15:05:51 +0300

    minor syntax change

--- src/ndlib/types.h
+++ src/ndlib/types.h
@@ -16,7 +16,7 @@ namespace laruen::ndlib {
     using Shape = std::vector<uint_fast64_t>;
     using Strides = std::vector<uint_fast64_t>;
     using NDIndex = std::vector<uint_fast64_t>;
-    using SliceRanges = std::vector<laruen::ndlib::Range<uint_fast64_t>>;
+    using SliceRanges = std::vector<Range<uint_fast64_t>>;
     using Axes = std::vector<uint_fast8_t>;
 
     namespace types {
