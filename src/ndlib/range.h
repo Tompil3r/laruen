18f3a30264304f68cf958ba211b57cf06244a5a4
Tompil3r <tompil3r@gmail.com>
2022-06-23 15:42:28 +0300

    moved range.tpp into range.h

--- src/ndlib/range.h
+++ src/ndlib/range.h
@@ -3,6 +3,7 @@
 #define NDLIB_RANGE_H_
 
 #include <string>
+#include <ostream>
 
 namespace laruen::ndlib {
     
@@ -11,12 +12,27 @@ namespace laruen::ndlib {
         T end;
         T step;
 
-        constexpr Range(T end) noexcept;
-        constexpr Range(T start, T end) noexcept;
-        constexpr Range(T start, T end, T step) noexcept;
-        std::string str() const noexcept;
-    };
-};
+        inline constexpr Range(T end) noexcept
+        : start(0), end(end), step(1)
+        {}
 
-#include "src/ndlib/range.tpp"
+        inline constexpr Range(T start, T end) noexcept
+        : start(start), end(end), step(1)
+        {}
+
+        inline constexpr Range(T start, T end, T step) noexcept
+        : start(start), end(end), step(step)
+        {}
+        
+        std::string str() const noexcept {
+            return std::to_string(this->start) + ':' +
+            std::to_string(this->end) + ':' + std::to_string(this->step);
+        }
+
+        friend inline std::ostream& operator<<(std::ostream &stream, const Range<T> &range) noexcept {
+            return stream << range.str();
+        }
+    };
+}
+
 #endif
