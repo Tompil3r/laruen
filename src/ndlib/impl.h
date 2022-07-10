9f38d0ac867fc572052d744aee19236a9b2254ac
Tompil3r <tompil3r@gmail.com>
2022-07-10 21:18:55 +0300

    added dot_1d function to impl.h

--- src/ndlib/impl.h
+++ src/ndlib/impl.h
@@ -1,7 +1,22 @@
 #ifndef NDLIB_STATIC_H_
 #define NDLIB_STATIC_H_
 
+#include <cstdint>
+
 namespace laruen::ndlib::impl {
+
+    template <typename TR, typename T, typename T2>
+    inline TR dot_1d(T *lhs_ptr, uint_fast64_t lhs_stride,
+    T2 *rhs_ptr, uint_fast64_t rhs_stride, uint_fast64_t size) noexcept {
+        TR product = 0;
+        for(uint_fast64_t i = 0;i < size;i++) {
+            product += (*lhs_ptr) * (*rhs_ptr);
+            lhs_ptr += lhs_stride;
+            rhs_ptr += rhs_stride;
+        }
+
+        return product;
+    }
 }
 
 #endif
