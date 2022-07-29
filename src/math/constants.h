d7d36197e81d8d5506583e2c4f624d583f35f8d0
Tompil3r <tompil3r@gmail.com>
2022-07-29 17:12:56 +0300

    added math constants

---
+++ src/math/constants.h
@@ -0,0 +1,14 @@
+
+#ifndef MATH_CONSTANTS_H_
+#define MATH_CONSTANTS_H_
+
+#include <cmath>
+
+namespace laruen::math::constants {
+
+    constexpr std::double_t EULERS_NUMBER = 2.71828182845904523536;
+    constexpr std::double_t PI = 3.14159265358979323846;
+
+}
+
+#endif
