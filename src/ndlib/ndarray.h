506bac9c2f96c5a88f6f0aec807b425637b36e85
Tompil3r <tompil3r@gmail.com>
2022-08-05 19:51:36 +0300

    added inverse shr to NDArray

--- src/ndlib/ndarray.h
+++ src/ndlib/ndarray.h
@@ -1511,6 +1511,48 @@ namespace laruen::ndlib {
             }
             
             template <typename T2>
+            inline NDArray& inverse_shr_eq(const NDArray<T2> &rhs) noexcept {
+                Impl::inverse_shr_eq(this->data_, *this, rhs.data_,
+                this->shape_ == rhs.shape_ ? rhs : rhs.expansion(*this));
+                return *this;
+            }
+
+            inline NDArray& inverse_shr_eq(T value) noexcept {
+                Impl::inverse_shr_eq(this->data_, *this, value);
+                return *this;
+            }
+
+            template <typename T2, typename TR>
+            inline NDArray<TR>& inverse_shr(const NDArray<T2> &rhs, NDArray<TR> &out) const noexcept {
+                Impl::inverse_shr(this->data_, this->shape_ == out.shape_ ? *this : this->expansion(out),
+                rhs.data_, rhs.shape_ == out.shape_ ? rhs : rhs.expansion(out),
+                out.data_, out);
+                return out;
+            }
+
+            template <typename TR>
+            inline NDArray<TR>& inverse_shr(T value, NDArray<TR> &out) const noexcept {
+                Impl::inverse_shr(this->data_,
+                this->shape_ == out.shape_ ? *this : this->expansion(out),
+                value, out.data_, out);
+                return out;
+            }
+
+            template <typename TR, typename T2>
+            inline NDArray<TR> inverse_shr(const NDArray<T2> &rhs) const noexcept {
+                NDArray<TR> out(laruen::ndlib::utils::broadcast(this->shape_, rhs.shape_));
+                this->inverse_shr(rhs, out);
+                return out;
+            }
+
+            template <typename TR>
+            inline NDArray<TR> inverse_shr(TR value) const noexcept {
+                NDArray<TR> out(new TR[this->size_], *this, nullptr);
+                this->inverse_shr(value, out);
+                return out;
+            }
+            
+            template <typename T2>
             inline NDArray& inverse_remainder_eq(const NDArray<T2> &rhs) noexcept {
                 Impl::inverse_remainder_eq(this->data_, *this, rhs.data_,
                 this->shape_ == rhs.shape_ ? rhs : rhs.expansion(*this));
