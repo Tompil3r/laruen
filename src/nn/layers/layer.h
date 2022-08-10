e348b6fc75b37c06e05f323f0836d9c0c79343aa
Tompil3r <tompil3r@gmail.com>
2022-08-10 17:59:56 +0300

    initialized Layer class

--- src/nn/layers/layer.h
+++ src/nn/layers/layer.h
@@ -2,11 +2,26 @@
 #ifndef NN_LAYERS_LAYER_H_
 #define NN_LAYERS_LAYER_H_
 
+#include <cassert>
+#include <utility>
+#include "src/ndlib/ndarray.h"
+#include "src/ndlib/types.h"
+
 namespace laruen::nn::layers {
 
-    class Layer {
+    namespace impl {
+        using laruen::ndlib::float32_t;
 
-    };
+        template <typename T = float32_t>
+        class Layer {
+
+            public:
+                void forward() const noexcept = 0;
+                void backward() const noexcept = 0;
+        };
+    }
+
+    using namespace impl;
 }
 
 
