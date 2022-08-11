3092e091e2c9b16dff11de623623f65ba42f251a
Tompil3r <tompil3r@gmail.com>
2022-08-11 13:45:16 +0300

    added ReLU class

---
+++ src/nn/layers/relu.h
@@ -0,0 +1,28 @@
+
+#ifndef NN_LAYERS_RELU_H_
+#define NN_LAYERS_RELU_H_
+
+
+#include "src/nn/layers/layer.h"
+
+
+namespace laruen::nn::layers {
+    namespace impl {
+
+        template <typename T>
+        class ReLU : public Layer<T> {
+            public:
+                NDArray<T>& forward(const NDArray<T> &input, NDArray<T> &output) const override final {
+                    input.maximum(0, output);
+                    return output;
+                }
+
+                void backward() const noexcept override final {
+                }
+        };
+    }
+
+    using namespace impl;
+}
+
+#endif
