d57b86f5e45eae1eacc8e4034582a6ef0b77037a
Tompil3r <tompil3r@gmail.com>
2022-08-11 13:50:57 +0300

    added missing include

--- src/nn/layers/relu.h
+++ src/nn/layers/relu.h
@@ -2,12 +2,14 @@
 #ifndef NN_LAYERS_RELU_H_
 #define NN_LAYERS_RELU_H_
 
-
+#include "src/ndlib/ndarray.h"
 #include "src/nn/layers/layer.h"
 
 
 namespace laruen::nn::layers {
     namespace impl {
+
+        using laruen::ndlib::NDArray;
 
         template <typename T>
         class ReLU : public Layer<T> {
