484c1693d87981ba6b870dea16962d9b2f1ff73f
Tompil3r <tompil3r@gmail.com>
2022-08-19 17:11:50 +0300

    added nn/utils.h

---
+++ src/nn/utils.h
@@ -0,0 +1,27 @@
+
+#ifndef NN_UTILS_H_
+#define NN_UTILS_H_
+
+#include <cstdint>
+#include <algorithm>
+#include "src/ndlib/types.h"
+
+namespace laruen::nn::utils {
+
+    namespace impl {
+
+        using laruen::ndlib::Shape;
+
+        Shape batch_shape(const Shape &shape, uint_fast64_t batch_size) noexcept {
+            Shape result(shape.size() + 1);
+            std::copy(shape.cbegin(), shape.cend(), result.begin() + 1);
+            result.front() = batch_size;
+
+            return result;
+        }
+    }
+
+    using namespace impl;
+}
+
+#endif
