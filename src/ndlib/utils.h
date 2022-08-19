038642a3a9c6b1dad7b3b5083ab98a3a3de7347c
Tompil3r <tompil3r@gmail.com>
2022-08-19 16:48:44 +0300

    added impl namespace to ndlib/utils.h

--- src/ndlib/utils.h
+++ src/ndlib/utils.h
@@ -9,83 +9,89 @@
 
 namespace laruen::ndlib::utils {
 
-    using laruen::ndlib::Shape, laruen::ndlib::Axes;
+    namespace impl {
 
-    Shape broadcast_(const Shape &lhs, const Shape &rhs) {
-        // assume lhs.size() >= rhs.size()
+        using laruen::ndlib::Shape;
+        using laruen::ndlib::Axes;
 
-        Shape bshape(lhs);
-        const uint_fast8_t min_ndim = rhs.size();
-        const uint_fast8_t ndim_delta = lhs.size() - min_ndim;
-        uint_fast8_t imax, lval, rval;
+        Shape broadcast_(const Shape &lhs, const Shape &rhs) {
+            // assume lhs.size() >= rhs.size()
 
-        for(uint_fast8_t imin = 0;imin < min_ndim;imin++) {
-            uint_fast8_t imax = imin + ndim_delta;
-            lval = lhs[imax];
-            rval = rhs[imin];
+            Shape bshape(lhs);
+            const uint_fast8_t min_ndim = rhs.size();
+            const uint_fast8_t ndim_delta = lhs.size() - min_ndim;
+            uint_fast8_t imax, lval, rval;
 
-            if(lval != rval && lval != 1 && rval != 1) {
-                throw std::invalid_argument("shapes cannot be broadcasted");
+            for(uint_fast8_t imin = 0;imin < min_ndim;imin++) {
+                uint_fast8_t imax = imin + ndim_delta;
+                lval = lhs[imax];
+                rval = rhs[imin];
+
+                if(lval != rval && lval != 1 && rval != 1) {
+                    throw std::invalid_argument("shapes cannot be broadcasted");
+                }
+                bshape[imax] = laruen::math::common::max(lval, rval);
             }
-            bshape[imax] = laruen::math::common::max(lval, rval);
+
+            return bshape;
         }
 
-        return bshape;
-    }
+        Shape matmul_broadcast_(const Shape &lhs, const Shape &rhs) {
+            // assume lhs.size() >= rhs.size()
 
-    Shape matmul_broadcast_(const Shape &lhs, const Shape &rhs) {
-        // assume lhs.size() >= rhs.size()
+            Shape bshape(lhs);
+            const uint_fast8_t min_ndim = rhs.size();
+            const uint_fast8_t ndim_delta = lhs.size() - min_ndim;
+            uint_fast8_t imax, lval, rval;
 
-        Shape bshape(lhs);
-        const uint_fast8_t min_ndim = rhs.size();
-        const uint_fast8_t ndim_delta = lhs.size() - min_ndim;
-        uint_fast8_t imax, lval, rval;
+            for(uint_fast8_t imin = 0;imin < min_ndim - 2;imin++) {
+                uint_fast8_t imax = imin + ndim_delta;
+                lval = lhs[imax];
+                rval = rhs[imin];
 
-        for(uint_fast8_t imin = 0;imin < min_ndim - 2;imin++) {
-            uint_fast8_t imax = imin + ndim_delta;
-            lval = lhs[imax];
-            rval = rhs[imin];
-
-            if(lval != rval && lval != 1 && rval != 1) {
-                throw std::invalid_argument("shapes cannot be broadcasted");
+                if(lval != rval && lval != 1 && rval != 1) {
+                    throw std::invalid_argument("shapes cannot be broadcasted");
+                }
+                bshape[imax] = laruen::math::common::max(lval, rval);
             }
-            bshape[imax] = laruen::math::common::max(lval, rval);
+
+            bshape.back() = rhs.back();
+            return bshape;
         }
 
-        bshape.back() = rhs.back();
-        return bshape;
-    }
+        Axes compress_axes(const Axes &axes, uint_fast8_t ndim) {
+            Axes result(ndim - axes.size());
+            uint_fast8_t ridx = 0;
+            uint_fast8_t remaining_axes = -1;
 
-    Axes compress_axes(const Axes &axes, uint_fast8_t ndim) {
-        Axes result(ndim - axes.size());
-        uint_fast8_t ridx = 0;
-        uint_fast8_t remaining_axes = -1;
-
-        for(uint_fast8_t i = 0;i < axes.size();i++) {
-            remaining_axes ^= 1 << axes[i];
-        }
-
-        for(uint_fast8_t i = 0;i < ndim;i++) {
-            if(remaining_axes & (1 << i)) {
-                result[ridx] = i;
-                ridx++;
+            for(uint_fast8_t i = 0;i < axes.size();i++) {
+                remaining_axes ^= 1 << axes[i];
             }
+
+            for(uint_fast8_t i = 0;i < ndim;i++) {
+                if(remaining_axes & (1 << i)) {
+                    result[ridx] = i;
+                    ridx++;
+                }
+            }
+
+            return result;
         }
 
-        return result;
+        inline Shape broadcast(const Shape &lhs, const Shape &rhs) {
+            return lhs.size() >= rhs.size() ? broadcast_(lhs, rhs) : broadcast_(rhs, lhs);
+        }
+
+        inline Shape matmul_broadcast(const Shape &lhs, const Shape &rhs) {
+            return lhs.size() >= rhs.size() ? matmul_broadcast_(lhs, rhs) : matmul_broadcast_(rhs, lhs);
+        }
+
+        inline uint_fast64_t ceil_index(float64_t index) noexcept {
+            return (uint_fast64_t)index + ((uint_fast64_t)index < index);
+        }
     }
 
-    inline Shape broadcast(const Shape &lhs, const Shape &rhs) {
-        return lhs.size() >= rhs.size() ? broadcast_(lhs, rhs) : broadcast_(rhs, lhs);
-    }
-
-    inline Shape matmul_broadcast(const Shape &lhs, const Shape &rhs) {
-        return lhs.size() >= rhs.size() ? matmul_broadcast_(lhs, rhs) : matmul_broadcast_(rhs, lhs);
-    }
-
-    inline uint_fast64_t ceil_index(float64_t index) noexcept {
-        return (uint_fast64_t)index + ((uint_fast64_t)index < index);
-    }
+    using namespace impl;
 }
 
 
