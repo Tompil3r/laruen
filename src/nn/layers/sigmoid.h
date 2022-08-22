2a6260e302d1abe606ae1afbec09663e2ff6c327
Tompil3r <tompil3r@gmail.com>
2022-08-22 18:27:06 +0300

    implemented Sigmoid::backward

--- src/nn/layers/sigmoid.h
+++ src/nn/layers/sigmoid.h
@@ -2,8 +2,10 @@
 #ifndef NN_LAYERS_SIGMOID_H_
 #define NN_LAYERS_SIGMOID_H_
 
+#include <cassert>
 #include "src/ndlib/ndarray.h"
 #include "src/ndlib/types.h"
+#include "src/ndlib/nditer.h"
 #include "src/nn/layers/layer.h"
 
 namespace laruen::nn::layers {
@@ -11,6 +13,7 @@ namespace laruen::nn::layers {
     namespace impl {
         using laruen::ndlib::NDArray;
         using laruen::ndlib::float32_t;
+        using laruen::ndlib::NDIter;
 
         template <typename T = float32_t>
         class Sigmoid : public Layer<T> {
@@ -27,10 +30,33 @@ namespace laruen::nn::layers {
                     return output;
                 }
 
+                /**
+                 * @brief calculates the gradient of the Loss function with respect to Z
+                 * @param deriv dA (dL / dA) (dL = dLoss)
+                 * @param cached_input Z - not used in this case since (dL / dZ) can be
+                 * calculated through A (Sigmoid(Z)) more efficiently -> dS(z)/dZ = S(Z) * (1 - S(Z))
+                 * @param cached_output A (Sigmoid(Z))
+                 * @param prev_deriv_output dZ (dL / dZ)
+                 */
                 void backward(const NDArray<T> &deriv, const NDArray<T> &cached_input,
                 const NDArray<T> &cached_output, NDArray<T> &prev_deriv_output) noexcept override final
                 {
+                    assert(deriv.shape() == prev_deriv_output.shape());
 
+                    NDIter deriv_iter(deriv.data(), deriv);
+                    NDIter cached_output_iter(cached_output.data(), cached_output);
+                    NDIter output_iter(prev_deriv_output.data(), prev_deriv_output);
+
+                    /*
+                        (dL / dZ)[i] = (dL / dA)[i] * A[i] * (1 - A[i])
+                        ** [i] denotes the i'th element
+                    */
+                    for(uint_fast64_t i = 0;i < deriv.size();i++) {
+                        output_iter.next() = deriv_iter.next() * cached_output_iter.current()
+                        * (1 - cached_output_iter.current());
+
+                        cached_output_iter.next();
+                    }
                 }
 
                 void build(const Shape &input_shape) override final {
