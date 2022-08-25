1bf0f154f7ac920e4cab1de1700879c678e4ee0c
Tompil3r <tompil3r@gmail.com>
2022-08-25 15:34:45 +0300

    created Optimizer class

---
+++ src/nn/optimizers/optimizer.h
@@ -0,0 +1,26 @@
+
+#ifndef NN_OPTIMIZERS_OPTIMIZER_H_
+#define NN_OPTIMIZERS_OPTIMIZER_H_
+
+#include "src/ndlib/ndarray.h"
+#include "src/ndlib/types.h"
+
+namespace laruen::nn::optimizers {
+
+    namespace impl {
+        using laruen::ndlib::NDArray;
+        using laruen::ndlib::float32_t;
+
+        template <typename T = float32_t>
+        class Optimizer {
+            public:
+                void update(NDArray<T> &weights, const NDArray<T> &gradients) = 0;
+        };
+
+    }
+
+    using namespace impl;
+}
+
+
+#endif
