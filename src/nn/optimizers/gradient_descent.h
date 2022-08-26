6594ad75daba378f98c77ff4ee48e7ff4cbf7382
Tompil3r <tompil3r@gmail.com>
2022-08-26 21:52:58 +0300

    moved member variable 'learning_rate' to Optimizer class

--- src/nn/optimizers/gradient_descent.h
+++ src/nn/optimizers/gradient_descent.h
@@ -14,25 +14,14 @@ namespace laruen::nn::optimizers {
 
         template <typename T = float32_t>
         class GradientDescent : public Optimizer<T> {
-            private:
-                T learning_rate_;
-
             public:
                 GradientDescent(T learning_rate = 0.01f) noexcept
-                : learning_rate_(learning_rate)
+                : Optimizer<T>(learning_rate)
                 {}
 
                 void update(NDArray<T> &weights, NDArray<T> &raw_gradients, NDArray<T> &final_gradients) override final {
                     raw_gradients.multiply(this->learning_rate_, final_gradients);
                     weights.subtract_eq(final_gradients);
-                }
-
-                inline T learning_rate() const noexcept {
-                    return this->learning_rate_;
-                }
-
-                inline void learning_rate(T learning_rate) noexcept {
-                    this->learning_rate_ = learning_rate;
                 }
         };
     }
--- src/nn/optimizers/optimizer.h
+++ src/nn/optimizers/optimizer.h
@@ -13,8 +13,23 @@ namespace laruen::nn::optimizers {
 
         template <typename T = float32_t>
         class Optimizer {
+            protected:
+                T learning_rate_;
+
             public:
+                Optimizer(T learning_rate) noexcept
+                : learning_rate_(learning_rate)
+                {}
+
                 virtual void update(NDArray<T> &weights, NDArray<T> &raw_gradients, NDArray<T> &final_gradients) = 0;
+
+                inline T learning_rate() const noexcept {
+                    return this->learning_rate_;
+                }
+
+                inline void learning_rate(T learning_rate) noexcept {
+                    this->learning_rate_ = learning_rate;
+                }
         };
 
     }
