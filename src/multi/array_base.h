5135d93c649a70594556285c016d2670dba65b03
Tompil3r <tompil3r@gmail.com>
2022-08-27 18:14:17 +0300

    fixed syntax error

--- src/multi/array_base.h
+++ src/multi/array_base.h
@@ -27,7 +27,8 @@ namespace laruen::multi {
 
         public:
             ArrayBase() noexcept
-            : size_(0), ndim_(0), contig_(true);
+            : size_(0), ndim_(0), contig_(true)
+            {}
 
             ArrayBase(const Shape &shape, const Strides &strides, const Strides &dim_sizes,
             uint_fast64_t size, uint_fast8_t ndim, bool contig) noexcept
