62385ad789d77d6380518aa97350316631e2e521
Tompil3r <tompil3r@gmail.com>
2022-08-31 23:00:22 +0300

    added Metric class

---
+++ src/nn/metrics/metric.h
@@ -0,0 +1,25 @@
+
+#ifndef NN_METRICS_METRIC_H_
+#define NN_METRICS_METRIC_H_
+
+#include "src/multi/ndarray.h"
+#include "src/multi/types.h"
+
+namespace laruen::nn::metrics {
+
+    namespace impl {
+
+        using laruen::multi::NDArray;
+        using laruen::multi::float32_t;
+
+        template <typename T = float32_t>
+        class Metric {
+            public:
+                virtual T operator()(const NDArray<T> &y_true, const NDArray<T> &y_pred) const = 0;
+        };
+    }
+
+    using namespace impl;
+}
+
+#endif
