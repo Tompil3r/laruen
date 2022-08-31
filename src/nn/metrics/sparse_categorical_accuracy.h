7e3a6c426f88d01c2a9c9e483630989eecc83a81
Tompil3r <tompil3r@gmail.com>
2022-08-31 23:34:36 +0300

    added SparseCategoricalAccuracy

---
+++ src/nn/metrics/sparse_categorical_accuracy.h
@@ -0,0 +1,51 @@
+
+#ifndef NN_METRICS_SPARSE_CATEGORICAL_ACCURACY_H_
+#define NN_METRICS_SPARSE_CATEGORICAL_ACCURACY_H_
+
+#include "src/multi/ndarray.h"
+#include "src/multi/types.h"
+#include "src/multi/nditer.h"
+#include "src/nn/metrics/metric.h"
+
+namespace laruen::nn::metrics {
+
+    using laruen::multi::NDArray;
+    using laruen::multi::float32_t;
+    using laruen::multi::NDIter;
+
+    namespace impl {
+        
+        template <typename T = float32_t>
+        class SparseCategoricalAccuracy : public Metric<T> {            
+            public:
+                T operator()(const NDArray<T> &y_true, const NDArray<T> &y_pred) const override final {
+                    NDIter true_iter(y_true.data(), y_true);
+                    NDIter pred_iter(y_pred.data(), y_pred);
+
+                    T score = 0;
+
+                    for(uint_fast64_t i = 0;i < y_true.size();i++) {
+                        T max = pred_iter.next();
+                        uint_fast64_t index_max = 0;
+
+                        for(uint_fast64_t j = 1;j < y_pred.shape().back();j++) {
+                            if(pred_iter.current() > max) {
+                                max = pred_iter.current();
+                                index_max = j;
+                            }
+                            pred_iter.next();
+                        }
+                        score += true_iter.next() == index_max;
+                    }
+
+                    return (score / y_true.size());
+                }
+        };
+    }
+
+    using namespace impl;
+}
+
+
+
+#endif
