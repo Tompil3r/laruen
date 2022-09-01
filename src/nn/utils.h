224bf5cbfc54761fb60fb73106e5df1468c93d96
Tompil3r <tompil3r@gmail.com>
2022-09-01 14:03:36 +0300

    added nn::utils::batch_view()

--- src/nn/utils.h
+++ src/nn/utils.h
@@ -4,13 +4,17 @@
 
 #include <cstdint>
 #include <algorithm>
+#include <utility>
+#include "src/multi/ndarray.h"
 #include "src/multi/types.h"
 
 namespace laruen::nn::utils {
 
     namespace impl {
 
+        using laruen::multi::NDArray;
         using laruen::multi::Shape;
+        using laruen::multi::Strides;
 
         Shape add_batch_shape(const Shape &shape, uint_fast64_t batch_size) noexcept {
             Shape result(shape.size() + 1);
@@ -24,6 +28,19 @@ namespace laruen::nn::utils {
             Shape result(shape);
             result.front() = batch_size;
             return result;
+        }
+
+        template <typename T>
+        NDArray<T> batch_view(NDArray<T> &ndarray, uint_fast64_t batch_size) {
+            Shape new_shape(ndarray.shape());
+            Strides new_dim_sizes(ndarray.dim_sizes());
+
+            uint_fast64_t new_size = (ndarray.size() / ndarray.shape().front()) * batch_size;
+            new_shape.front() = batch_size;
+            new_dim_sizes.front() = (new_dim_sizes.front() / ndarray.shape().front()) * batch_size;
+
+            return NDArray<T>(ndarray.data(), std::move(new_shape), Strides(ndarray.strides()),
+            std::move(new_dim_sizes), new_size, ndarray.ndim(), ndarray.contig(), false);
         }
     }
 
