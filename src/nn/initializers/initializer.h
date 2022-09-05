120ad92f022dbd5f506f1fb9b64fbc59fb517297
Tompil3r <tompil3r@gmail.com>
2022-09-05 22:11:09 +0300

    added Initializer class

---
+++ src/nn/initializers/initializer.h
@@ -0,0 +1,27 @@
+
+#ifndef LARUEN_NN_INITIALIZERS_INITIALIZER_H_
+#define LARUEN_NN_INITIALIZERS_INITIALIZER_H_
+
+#include <cstdint>
+#include "src/multi/ndarray.h"
+#include "src/multi/types.h"
+
+namespace laruen::nn::initializers {
+
+    namespace impl {
+        using laruen::multi::NDArray;
+        using laruen::multi::float32_t;
+
+        template <typename T = float32_t>
+        class Initializer {
+            public:
+                void operator()(uint_fast64_t nb_inputs, uint_fast64_t nb_nodes, NDArray<T> &output) const = 0;
+                NDArray<T> operator()(uint_fast64_t nb_inputs, uint_fast64_t nb_nodes) const = 0;
+        };
+    }
+
+    using namespace impl;
+}
+
+
+#endif
