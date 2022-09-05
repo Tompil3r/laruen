5d8dc6b892b7c4e1837aca8c984c704f28b99173
Tompil3r <tompil3r@gmail.com>
2022-09-05 20:09:19 +0300

    replaced Range with SlicingRange in definition of Slicings

--- src/multi/types.h
+++ src/multi/types.h
@@ -5,7 +5,7 @@
 #include <cstdint>
 #include <vector>
 #include <cmath>
-#include "src/multi/range.h"
+#include "src/multi/slicing_range.h"
 
 namespace laruen::multi {
     using float32_t = std::float_t;
@@ -14,7 +14,7 @@ namespace laruen::multi {
     using Shape = std::vector<uint_fast64_t>;
     using Strides = std::vector<uint_fast64_t>;
     using NDIndex = std::vector<uint_fast64_t>;
-    using Slicings = std::vector<Range<uint_fast64_t>>;
+    using Slicings = std::vector<SlicingRange>;
     using Axes = std::vector<uint_fast8_t>;
 }
 
