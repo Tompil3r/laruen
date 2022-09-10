4d2e952e503208c75084529602e562d8aff6f99f
Tompil3r <tompil3r@gmail.com>
2022-09-10 21:22:58 +0300

    added vector of values to Metric

--- laruen/nn/metrics/metric.h
+++ laruen/nn/metrics/metric.h
@@ -2,6 +2,7 @@
 #ifndef LARUEN_NN_METRICS_METRIC_H_
 #define LARUEN_NN_METRICS_METRIC_H_
 
+#include <vector>
 #include "laruen/multi/ndarray.h"
 #include "laruen/multi/types.h"
 
@@ -14,9 +15,20 @@ namespace laruen::nn::metrics {
 
         template <typename T = float32_t>
         class Metric {
+            private:
+                std::vector<T> values_;
+
             public:
                 virtual ~Metric()
                 {}
+
+                std::vector<T>& values() noexcept {
+                    return this->values_;
+                }
+
+                const std::vector<T>& values() const noexcept {
+                    return this->values_;
+                }
 
                 virtual T operator()(const NDArray<T> &y_true, const NDArray<T> &y_pred) const = 0;
                 virtual const char* name() const noexcept = 0;
