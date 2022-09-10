96c154c5ef232cf01f98e481eace8bd9fb24f733
Tompil3r <tompil3r@gmail.com>
2022-09-10 21:18:07 +0300

    added Callback class

---
+++ laruen/nn/callbacks/callback.h
@@ -0,0 +1,33 @@
+
+#ifndef LARUEN_NN_CALLBACKS_CALLBACK_H_
+#define LARUEN_NN_CALLBACKS_CALLBACK_H_
+
+#include "laruen/multi/ndarray.h"
+#include "laruen/multi/types.h"
+#include "laruen/nn/model.h"
+
+namespace laruen::nn::callbacks {
+
+    namespace impl {
+
+        using laruen::multi::NDArray;
+        using laruen::multi::float32_t;
+        using laruen::nn::Model;
+
+        template <typename T = float32_t>
+        class Callback {
+            private:
+                Model<T> *model_;
+
+            public:
+                virtual ~Callback()
+                {}
+
+                virtual void operator()() const = 0;
+        };
+    }
+
+    using namespace impl;
+}
+
+#endif
