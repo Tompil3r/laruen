df6bd3b31d7765ae18c1e52480e2973492404606
Tompil3r <tompil3r@gmail.com>
2022-09-18 18:58:46 +0300

    Callback fixes

--- laruen/nn/callbacks/callback.h
+++ laruen/nn/callbacks/callback.h
@@ -7,51 +7,58 @@
 #include "laruen/multi/types.h"
 #include "laruen/nn/model.h"
 
-namespace laruen::nn::callbacks {
+namespace laruen::nn {
 
     namespace impl {
-
-        using laruen::multi::NDArray;
-        using laruen::multi::float32_t;
-        using laruen::nn::Model;
-
-        template <typename T = float32_t>
-        class Callback {
-            private:
-                Model<T> *model_ = nullptr;
-                uint_fast8_t verbose_;
-
-            public:
-                virtual ~Callback()
-                {}
-
-                Callback(uint_fast8_t verbose = 1)
-                : verbose_(verbose)
-                {}
-
-                inline void model(Model<T> *model) noexcept {
-                    this->model_ = model;
-                }
-
-                inline Model<T>* model() noexcept {
-                    return this->model_;
-                }
-
-                inline void verbose_mode(uint_fast8_t verbose) {
-                    this->verbose_ = verbose;
-                }
-
-                inline uint_fast8_t verbose_mode() const {
-                    return this->verbose_;
-                }
-
-                virtual Callback<T>* clone() const = 0;
-
-                virtual void operator()(uint_fast64_t epoch) const = 0;
-        };
+        template <typename> class Model;
     }
 
-    using namespace impl;
+    namespace callbacks {
+
+        namespace impl {
+
+            using laruen::multi::NDArray;
+            using laruen::multi::float32_t;
+            using laruen::nn::Model;
+
+            template <typename T = float32_t>
+            class Callback {
+                protected:
+                    Model<T> *model_ = nullptr;
+                    uint_fast8_t verbose_;
+
+                public:
+                    virtual ~Callback()
+                    {}
+
+                    Callback(uint_fast8_t verbose = 1)
+                    : verbose_(verbose)
+                    {}
+
+                    inline void model(Model<T> *model) noexcept {
+                        this->model_ = model;
+                    }
+
+                    inline Model<T>* model() noexcept {
+                        return this->model_;
+                    }
+
+                    inline void verbose_mode(uint_fast8_t verbose) {
+                        this->verbose_ = verbose;
+                    }
+
+                    inline uint_fast8_t verbose_mode() const {
+                        return this->verbose_;
+                    }
+
+                    virtual Callback<T>* clone() const = 0;
+
+                    virtual void operator()(uint_fast64_t epoch) const = 0;
+            };
+        }
+
+        using namespace impl;
+    }
 }
 
 #endif
