c4a649a5f7e18a03a986a6f3480e1e31736036dc
Tompil3r <tompil3r@gmail.com>
2022-09-21 22:37:08 +0300

    added virtual dtor to Initializer

--- laruen/nn/initializers/initializer.h
+++ laruen/nn/initializers/initializer.h
@@ -15,6 +15,9 @@ namespace laruen::nn::initializers {
         template <typename T = float32_t>
         class Initializer {
             public:
+                virtual ~Initializer()
+                {}
+
                 virtual Initializer<T>* clone() const = 0;
 
                 virtual void operator()(uint_fast64_t nb_inputs, uint_fast64_t nb_nodes, NDArray<T> &output) = 0;
